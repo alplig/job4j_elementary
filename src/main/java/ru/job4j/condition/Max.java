45391be8a6ba20dc1885908741db5b08230a4822
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2023-03-31 17:49:38 +0400

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,8 +2,15 @@ package ru.job4j.condition;
 
 public class Max {
 
-        public static int max(int left, int right) {
-
-            return  left > right ? left : right;
+        public static int max(int a, int b) {
+            return  a > b ? a : b;
         }
+
+        public static int max(int a, int b, int c) {
+            return a > max(b, c) ? a : max(b, c);
+        }
+
+    public static int max(int a, int b, int c, int d) {
+        return a > max(b, c, d) ? a : max(b, c, d);
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,28 +6,49 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int a = 1;
+        int b = 2;
+        int result = Max.max(a, b);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenMax2To1Then2() {
-        int left = 2;
-        int right = 1;
-        int result = Max.max(left, right);
+        int a = 2;
+        int b = 1;
+        int result = Max.max(a, b);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenMax0To0Then2() {
-        int left = 0;
-        int right = 0;
-        int result = Max.max(left, right);
+        int a = 0;
+        int b = 0;
+        int result = Max.max(a, b);
         int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenA1B2C3Max3() {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int result = Max.max(a, b, c);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenA1B4C3D2Max3() {
+        int a = 1;
+        int b = 4;
+        int c = 3;
+        int d = 2;
+        int result = Max.max(a, b, c, d);
+        int expected = 4;
         assertThat(result).isEqualTo(expected);
     }
 }
